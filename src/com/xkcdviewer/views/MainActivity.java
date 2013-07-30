e7425f07d93143f1362a47a269630741fd1e323e
Asier Carreño <ascalotoru@gmail.com>
2013-07-30 21:01:54 +0200

    Gestures added.

--- src/com/xkcdviewer/views/MainActivity.java
+++ src/com/xkcdviewer/views/MainActivity.java
@@ -5,7 +5,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.ExecutionException;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -14,35 +13,39 @@ import com.xkcdviewer.data.Comic;
 import com.xkcdviewer.data.JSONParser;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.v4.view.MotionEventCompat;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnGestureListener{
 
 	private String LOG_TAG = "MainActivity";
 	
 	private ProgressBar pbarProgreso;
-	private ProgressDialog pDialog;
+	public static ProgressDialog pDialog;
 	private ImageView image;
 	private TextView title;
-	private Button btnPrevious;
 	private URL url;
+	private GestureDetector gDetector;
 	
 	private static String LAST_COMIC_URL = "http://xkcd.com/info.0.json";
 	private JSONParser jsonParser;
 	private int currentComic;
+	private int lastComic;
 	private Comic comic;
-	private JSONArray comics;
 	private JSONObject json;
 	private static final String TAG_MONTH = "month";
 	private static final String TAG_NUM = "num";
@@ -64,10 +67,10 @@ public class MainActivity extends Activity {
         image = (ImageView) findViewById(R.id.imagen);
         title = (TextView) findViewById(R.id.title);
         pbarProgreso = (ProgressBar)findViewById(R.id.pbarProgreso);
-        btnPrevious = (Button) findViewById(R.id.bntAnterior);
+        gDetector = new GestureDetector(this.getApplicationContext(), this);
         
         pDialog = new ProgressDialog(MainActivity.this);
-        pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        pDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         pDialog.setMessage("Procesando...");
         pDialog.setMax(100);
         
@@ -84,6 +87,7 @@ public class MainActivity extends Activity {
         
         try{
         	extractJSONData(json);
+        	lastComic = Integer.parseInt(comic.getNum());
         }catch (JSONException e) {
 			Log.e(this.LOG_TAG, "Error extracting data " + e.toString());
 		}
@@ -107,10 +111,10 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    public void showPrevious(View view){
+    public void showComic(int comicNumber){
     	jsonParser = new JSONParser(this.getApplicationContext());
     	try {
-			json = jsonParser.execute("http://xkcd.com/"+(currentComic-1)+
+			json = jsonParser.execute("http://xkcd.com/"+comicNumber+
 					"/info.0.json").get();
 			extractJSONData(json);
 			printTheData();
@@ -151,4 +155,60 @@ public class MainActivity extends Activity {
     	image.setImageBitmap(bmp);
     	title.setText(comic.getTitle());
     }
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		return gDetector.onTouchEvent(event);
+	}
+
+	@Override
+	public boolean onDown(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean onFling(MotionEvent start, MotionEvent finish, float velocityX,
+			float velocityY) {
+		if(start.getRawX() < finish.getRawX()){
+			// Previous
+			Log.d(this.LOG_TAG, "A la derecha el dedo.");
+			showComic(currentComic-1);
+		}else{
+			// Next
+			Log.d(this.LOG_TAG, "A la izquierda el dedo.");
+			if(currentComic>=lastComic){
+				Toast.makeText(this.getApplicationContext(), "Is the last comic."
+						, Toast.LENGTH_SHORT).show();
+			}else{	
+				showComic(currentComic+1);
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public void onLongPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
